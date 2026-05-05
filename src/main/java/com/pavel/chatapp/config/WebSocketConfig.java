a9990075aa212d06c5747a2aef257c9d4165e6b5
Aminjon <tashkenttashkent24@gmail.com>
2026-05-05 13:15:19 +0500

    fix: stabilize Spring Boot build for deployment

--- src/main/java/com/pavel/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/pavel/chatapp/config/WebSocketConfig.java
@@ -1,21 +1,19 @@
 package com.pavel.chatapp.config;
 
-import ch.qos.logback.classic.pattern.MessageConverter;
+
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.DefaultContentTypeResolver;
-import org.springframework.messaging.converter.JacksonJsonMessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
 
 import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
